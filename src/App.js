ab69d01c2beb71317d65c84cb24ceff3d9ac3921
cleave3 <owhiroroeghele@gmail.com>
2019-08-26 17:33:03 +0100

    updated App.js to use class based components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Sidebar from "./components/layout/Sidebar";
+import Main from "./components/layout/Main";
+import Footer from "./components/layout/Footer";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar/>
-      <div className="main-section">
-      <Sidebar/>
-      <Main/>
-      </div>
-      <Footer/>
-    </React.Fragment>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <div className="main-section">
+          <Sidebar />
+          <Main />
+        </div>
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
