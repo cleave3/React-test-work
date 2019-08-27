fd795899af2b3f1b786d833d45d651ea29d24ab2
cleave3 <owhiroroeghele@gmail.com>
2019-08-27 15:30:29 +0100

    build app

---
+++ build/precache-manifest.97433d2abefc9a1abebf7706e19dcb72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff22ae2f06aaa7d328b1e33b24d11417",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f271755a27b4af4c89b",
+    "url": "/static/css/main.a5e0f6ef.chunk.css"
+  },
+  {
+    "revision": "3fae0b53fb7489093a4e",
+    "url": "/static/js/2.7b1b2a26.chunk.js"
+  },
+  {
+    "revision": "6f271755a27b4af4c89b",
+    "url": "/static/js/main.4bd95f45.chunk.js"
+  },
+  {
+    "revision": "39f453967496343f2d8c",
+    "url": "/static/js/runtime~main.26cef8f3.js"
+  }
+]);
